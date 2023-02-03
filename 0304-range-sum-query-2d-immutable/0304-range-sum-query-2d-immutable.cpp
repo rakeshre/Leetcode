b062a3476ddf00e3e34a7f7ad4a461114bbacd34
rakeshre <38933118+rakeshre@users.noreply.github.com>
2023-02-03 15:09:28 -0800

    Time: 428 ms (68.37%), Space: 139.5 MB (98.91%) - LeetHub

--- 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.cpp
+++ 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.cpp
@@ -19,9 +19,11 @@ public:
     }
     
     int sumRegion(int row1, int col1, int row2, int col2) {
+        /*
         if(row1==0 && col1==0){
             return p_sum[row2][col2];
         }
+        */
         int ans= p_sum[row2][col2]-(col1>=1? p_sum[row2][col1-1]: 0)-(row1>=1? p_sum[row1-1][col2]:0)+((row1>=1 && col1>=1) ? p_sum[row1-1][col1-1]: 0);
         return ans;
     }
