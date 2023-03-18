22353c7126547a708f4af81446dbd4f6290a47bc
rakeshre <38933118+rakeshre@users.noreply.github.com>
2023-03-17 19:06:51 -0700

    Time: 168 ms (88.32%), Space: 83.2 MB (42.04%) - LeetHub

--- 2587-rearrange-array-to-maximize-prefix-score/2587-rearrange-array-to-maximize-prefix-score.cpp
+++ 2587-rearrange-array-to-maximize-prefix-score/2587-rearrange-array-to-maximize-prefix-score.cpp
@@ -1,17 +1,12 @@
 class Solution {
 public:
     int maxScore(vector<int>& nums) {
-        vector<long> pSum;
+        long sum=0;
         sort(nums.begin(),nums.end(),greater<int>());
         int n=nums.size();
         int ct=0;
-        if(nums[0]>0){
-            ct+=1;
-        }
-        pSum.push_back(nums[0]);
-        for(int i=1;i<n;++i){
-            long sum = nums[i]+pSum[i-1];
-            pSum.push_back(sum);
+        for(int i=0;i<n;++i){
+            sum += nums[i];
             if(sum>0){
                 ct+=1;
             }
