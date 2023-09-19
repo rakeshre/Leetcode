749a71952d2db586e9c2d27ce6a044f799a022ed
Rakesh Reddy Sanagala <38933118+rakeshre@users.noreply.github.com>
2023-09-19 14:42:26 -0700

    Time: 11 ms (79.16%), Space: 17.9 MB (8.59%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -1,45 +1,33 @@
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-//         int n=intervals.size();
-//         vector<vector<int>> intervals_new;
-//         int st=-1;
-//         for(int i=0;i<n;++i){
-//             if(intervals[i][1]<newInterval[0]){
-//                 intervals_new.push_back(intervals[i]);
-//             }else{
-//                 st=intervals[i][0];
-//             }
-            
-//         }
-//     }
-          vector<vector<int>>ans;
-
-        // 2 cases.
-        for(auto currentInterval:intervals){
-        //case 1 :non overlapping
-        // new interval is after current interval. add current interval to the ans.
-        if(currentInterval[1]<newInterval[0]){ans.push_back(currentInterval);}
-        // if new interval comes before current interval that means we need to make it current interval and check for overlaping with others. And our new interval will also be add to ans.
-        else if(newInterval[1]<currentInterval[0]){
+        vector<vector<int>> ans;
+        int high=-1;
+        for(int i=0;i<intervals.size();++i){
+            vector<int> curr=intervals[i];
+            //before new interval
+            if(curr[1]<newInterval[0]){
+                ans.push_back(curr);
+                high=curr[1];
+            }else if(curr[0]>newInterval[1]){
+                //after intervals
+                if(!newInterval.empty()){
                     ans.push_back(newInterval);
-                    //update it to new interval for overlapping checking.
-                    newInterval = currentInterval;
+                    newInterval={};
+                }
+                
+                ans.push_back(curr);
+            }else{
+                //overlapping
+                newInterval[0]=min(newInterval[0],curr[0]);
+                newInterval[1]=max(newInterval[1],curr[1]);
+                
+            }
         }
-        // case 2 :overlapping
-        else{
-            // update the newInterval for checking overlapping.
-            newInterval[0] = min(currentInterval[0],newInterval[0]);
-            newInterval[1] = max(currentInterval[1],newInterval[1]);
-
+        if(newInterval[0]>high && !newInterval.empty()){
+            ans.push_back(newInterval);
         }
-
-        }
-        // Add the last non overlaping interval (newInterval)
-        ans.push_back(newInterval);
-
         return ans;
-
 
     }
 };
