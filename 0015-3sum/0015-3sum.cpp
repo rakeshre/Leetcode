54e08140386286b15dcd9824037c5c6ddcac9ed2
Rakesh Reddy Sanagala <38933118+rakeshre@users.noreply.github.com>
2023-09-20 15:21:46 -0700

    Time: 1152 ms (15.97%), Space: 191.9 MB (9.60%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        set<vector<int>> u_s;
+        vector<vector<int>> res;
+        set<vector<int>> unique_triplets;
         int n=nums.size();
         sort(nums.begin(),nums.end());
-        for(int i=0;i<n;++i){
+        for(int i=0;i<n-2;++i){
             int j=i+1;
             int k=n-1;
             while(j<k){
-                if(nums[i]+nums[j]+nums[k]==0){
-                    u_s.insert({nums[i],nums[j],nums[k]});
-                    j++;
-                    k--;
-                }else if(nums[i]+nums[j]+nums[k]<0){
-                    j++;
-                }else{
-                    k--;
-                }
+               if(nums[i]+nums[j]+nums[k]==0){
+                   unique_triplets.insert({nums[i],nums[j],nums[k]});
+                   j++;
+                   k--;
+               }else if(nums[i]+nums[j]+nums[k]<0){
+                   j++;
+               }else{
+                   k--;
+               }
             }
         }
-        vector<vector<int>> res;
-        for(auto x:u_s){
-            res.push_back(x);
+        for(auto entry:unique_triplets){
+            res.push_back(entry);
         }
         return res;
     }
