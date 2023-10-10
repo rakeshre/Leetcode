530fd7ae5a2e10a905322a93529bc6e30dfb92c2
Rakesh Reddy Sanagala <38933118+rakeshre@users.noreply.github.com>
2023-10-10 12:12:27 -0700

    Time: 2 ms (56.31%), Space: 7.5 MB (59.15%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,21 +1,29 @@
 class Solution {
 public:
-    void dfs(vector<int>& nums, int i, vector<int> &sub_set, vector<vector<int>> &res){
+    // void dfs(vector<int>& nums, int i, vector<int> &sub_set, vector<vector<int>> &res){
+    //     int n=nums.size();
+    //     if(i==n){
+    //         res.push_back(sub_set);
+    //         return;
+    //     }
+    //     sub_set.push_back(nums[i]);
+    //     dfs(nums, i+1, sub_set, res);
+    //     sub_set.pop_back();
+    //     dfs(nums, i+1, sub_set, res);
+    // }
+    void dfs(vector<int>& nums, int j, vector<int> &sub_set, vector<vector<int>> &res){
         int n=nums.size();
-        if(i==n){
+        for(int i=j;i<n;++i){
+            sub_set.push_back(nums[i]);
             res.push_back(sub_set);
-            return;
+            dfs(nums, i+1, sub_set, res);
+            sub_set.pop_back();
         }
-        sub_set.push_back(nums[i]);
-        dfs(nums, i+1, sub_set, res);
-        sub_set.pop_back();
-        dfs(nums, i+1, sub_set, res);
     }
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>> res;
-        int n=nums.size();
         vector<int> emp;
-        //result.push_back(emp);
+        res.push_back(emp);
         dfs(nums, 0, emp, res);
         return res;
     }
